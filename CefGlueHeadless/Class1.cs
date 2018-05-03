a64164c970123d6b31b0a82ee301f53c1bb4d56b
Alexei White <alexei.white@gmail.com>
2018-05-03 15:22:44 -0700

    Initial Commit

---
+++ CefGlueHeadless/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefGlueHeadless
+{
+    public class Class1
+    {
+    }
+}
